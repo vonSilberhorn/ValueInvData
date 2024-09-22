37becb273f4091a86daffbbc4d5e950f795df4ea
Zoltán Szilberhorn <zoltan.szilberhorn@gmail.com>
2024-09-22 16:01:00 +0700

    ValueInvData #2: Add and configure logging

--- services/CompanyValuationService/src/main/java/com/szilberhornz/valueinvdata/services/companyvaluation/Main.java
+++
@@ -1,7 +0,0 @@
-package com.szilberhornz.valueinvdata.services.companyvaluation;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ services/StockValuationService/src/main/java/com/szilberhornz/valueinvdata/services/stockvaluation/StockValuationService.java
@@ -0,0 +1,14 @@
+package com.szilberhornz.valueinvdata.services.stockvaluation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StockValuationService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockValuationService.class);
+
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world! This is a new demo service called {}", StockValuationService.class.getSimpleName());
+    }
+}
