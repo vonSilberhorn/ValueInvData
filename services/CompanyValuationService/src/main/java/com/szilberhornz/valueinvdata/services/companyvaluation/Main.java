9bca1a566c073713f148a744a083b96aed3e9a1f
Zoltán Szilberhorn <zoltan.szilberhorn@gmail.com>
2024-09-22 15:00:08 +0700

    ValueInvData #1: Add initial project structure

---
+++ services/CompanyValuationService/src/main/java/com/szilberhornz/valueinvdata/services/companyvaluation/Main.java
@@ -0,0 +1,7 @@
+package com.szilberhornz.valueinvdata.services.companyvaluation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
