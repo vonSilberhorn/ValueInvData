6acf3ee936f4f58ba11d8a11a64662497ccb3f01
Zoltán Szilberhorn <zoltan.szilberhorn@gmail.com>
2024-09-23 20:32:10 +0700

    ValueInvData #5 add FMP authorization, add test dependencies and switch to Java 21 LTS version

---
+++ services/StockValuationService/src/test/java/com/szilberhornz/valueinvdata/services/stockvaluation/fmp/authr/JVMBasedFMPAuthorizationTest.java
@@ -0,0 +1,28 @@
+package com.szilberhornz.valueinvdata.services.stockvaluation.fmp.authr;
+
+import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ClearSystemProperty;
+import org.junitpioneer.jupiter.SetSystemProperty;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JVMBasedFMPAuthorizationTest {
+
+    final FMPAuthorization sut = new JVMBasedFMPAuthorization();
+
+    @Test
+    @ClearSystemProperty(key = "FMP_API_KEY")
+    void retrieveApiKeyShouldThrowException() {
+        final Exception exception = assertThrows(NoApiKeyFoundException.class, sut::retrieveApiKey);
+        assertEquals("No api key was set for the Financial Modeling Prep api!\n" +
+                "Please set it via the -DFMP_API_KEY VM option or if you don't yet have one, get one first at https://site.financialmodelingprep.com/developer/docs", exception.getMessage());
+    }
+
+    @Test
+    @SetSystemProperty(key = "FMP_API_KEY", value = "abcdef")
+    void retrieveApiKeyShouldSucceed() throws NoApiKeyFoundException {
+        final char[] result = sut.retrieveApiKey();
+        assertEquals("abcdef", new String(result));
+    }
+}
