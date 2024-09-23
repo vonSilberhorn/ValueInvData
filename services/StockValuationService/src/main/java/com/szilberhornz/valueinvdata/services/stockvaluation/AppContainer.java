7de9492e1581f810d098b906d6a2ad87360f341d
Zoltán Szilberhorn <zoltan.szilberhorn@gmail.com>
2024-09-23 22:26:22 +0700

    ValueInvData #6 AppContainer and AppContext classes

---
+++ services/StockValuationService/src/main/java/com/szilberhornz/valueinvdata/services/stockvaluation/AppContainer.java
@@ -0,0 +1,9 @@
+package com.szilberhornz.valueinvdata.services.stockvaluation;
+
+/**
+ * This class is a simple inversion of control container, responsible for managing the class instances
+ * of the application and providing dependency injection.
+ * Note: this is a kind of functionality that frameworks like Spring provide out of the box.
+ */
+public final class AppContainer {
+}
---
+++ services/StockValuationService/src/main/java/com/szilberhornz/valueinvdata/services/stockvaluation/AppContext.java
@@ -0,0 +1,10 @@
+package com.szilberhornz.valueinvdata.services.stockvaluation;
+
+/**
+ * This class is a collection of static variables, some of which are coming from VMOptions.
+ * I chose not to use any files to hold AppContext values because a) it's a small app,
+ * b) even though it's not a concern, file changes need releases while command line changes
+ * only need a bounce to take effect.
+ */
+public final class AppContext {
+}
