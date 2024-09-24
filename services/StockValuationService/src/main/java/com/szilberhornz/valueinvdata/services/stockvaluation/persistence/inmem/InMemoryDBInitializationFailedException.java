3936087ad445944c7483bfd3af072df96ee42d2d
Zoltán Szilberhorn <zoltan.szilberhorn@gmail.com>
2024-09-24 22:29:53 +0700

    ValueInvData #20 remove unused ctor

--- services/StockValuationService/src/main/java/com/szilberhornz/valueinvdata/services/stockvaluation/persistence/inmem/InMemoryDBInitializationFailedException.java
+++ services/StockValuationService/src/main/java/com/szilberhornz/valueinvdata/services/stockvaluation/persistence/inmem/InMemoryDBInitializationFailedException.java
@@ -2,10 +2,6 @@ package com.szilberhornz.valueinvdata.services.stockvaluation.persistence.inmem;
 
 public class InMemoryDBInitializationFailedException extends RuntimeException {
 
-    public InMemoryDBInitializationFailedException(String message) {
-        super(message);
-    }
-
     public InMemoryDBInitializationFailedException(String message, Throwable cause) {
         super(message, cause);
     }
